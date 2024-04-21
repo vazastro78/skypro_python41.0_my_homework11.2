89ce4ff831474e04eda34c7a7d585fc06a57ecdb
Valeev Azamat <azamat@mail.sao.ru>
2024-04-21 18:12:37 +0300

    uppercase function with docstring and new function capitalize with docstring

--- str_func.py
+++ str_func.py
@@ -1,2 +1,11 @@
 def uppercase(text: str):
+    '''
+    local function long description uppercase of input text
+    '''
     return text.upper()
+
+def capitalize(text: str):
+    '''
+    local function long description capitalize of input text
+    '''
+    return " ".join([item_text.capitalize() for item_text in text.split(" ")])
